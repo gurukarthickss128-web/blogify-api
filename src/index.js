5aa5b2e6e76ffd3c9cb9d162504d4b6269d80bf2
gurukarthickss128-web <gurukarthick.s.s.128@kalvium.community>
2026-03-12 14:13:27 +0530

    feat: modularize post routes using express.Router

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-const http = require('http');
-// The developer is trying to import the logMessage function
-// using destructuring, assuming an object is returned.
-const { logMessage } = require('./utils/log');
-
-const server = http.createServer((req, res) => {
-  // This line will crash the server because logMessage is undefined.
-  logMessage('A new request was received!'); 
-
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!');
-});
-
+const express = require('express');
+const app = express();
 const PORT = 3000;
 
-server.listen(PORT, () => {
-  console.log(`Server is attempting to run at http://localhost:${PORT}/`);
+const postRouter = require('./routes/posts.routes');
+
+// Welcome route
+app.get('/', (req, res) => {
+  res.send('Welcome to Blogify API!');
+});
+
+// Mount router
+app.use('/api/v1/posts', postRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
