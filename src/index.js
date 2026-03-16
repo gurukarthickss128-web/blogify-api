eeacfa293297b10092148f1ee658bece62a18f4b
gurukarthickss128-web <gurukarthick.s.s.128@kalvium.community>
2026-03-16 09:44:08 +0530

    feat: add endpoint to get post by id

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+
 const postRouter = require('./routes/posts.routes');
+const userRouter = require('./routes/users.routes');
 
 // Welcome route
 app.get('/', (req, res) => {
@@ -11,6 +13,7 @@ app.get('/', (req, res) => {
 
 // Mount router
 app.use('/api/v1/posts', postRouter);
+app.use('/api/v1/users', userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
